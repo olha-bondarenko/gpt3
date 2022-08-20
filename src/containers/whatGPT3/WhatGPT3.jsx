a797f62ad3ceee9b5c2b88b95950bc06204688fe
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-08-20 21:57:29 +0200

    wip: header, brand, about added

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatGPT3.css'
+
+const whatGPT3Text = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+const chatbotText = 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. '
+const knowledgebaseText = 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+const educationText = 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
 
 const WhatGPT3 = () => {
   return (
-    <div>whatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature title='What is GPT-3' text={whatGPT3Text}/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+      <Feature title='Chatbots' text={chatbotText}/>
+      <Feature title='Knowledgebase' text={knowledgebaseText}/>
+      <Feature title='Education' text={educationText}/>
+      </div>
+    </div>
   )
 }
 
